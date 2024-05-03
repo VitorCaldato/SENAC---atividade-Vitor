630c6e018390480713d1024ddcd345219e2e4c62
Vitor <167485600+VitorCaldato@users.noreply.github.com>
2024-05-03 15:40:56 -0300

    Update Cdetermine_numero.java

--- Cdetermine_numero.java
+++ Cdetermine_numero.java
@@ -3,7 +3,18 @@ public class Cdetermine_numero {
 
     public static void main(String[] args) {            // algoritmo que solicita ao usuario numero e determina se ele é positivo, negativo ou zero.
         num = int;
+        System.out.println( " Digite um numero" );
+        Scanner.scan.in = new Scan;
+            scan=num;
+            if{ 
+                num % 2;
+        Scanner.Scan ("Este número é positivo" );
+                }
+        if {
+            num == 0 ;
+            Scanner.scan (" Este numero é zero" )
+                {
+    if {
         
-
-    }
+            }
 }
